30197db35553d1633a631e414061567866b3e90c
zzy <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-04-10 14:33:01 +0800

    Update standard_callbacks.py

--- training/standard_callbacks.py
+++ training/standard_callbacks.py
@@ -68,6 +68,9 @@ def create_eval_callback(eval_name: str, loader: DataLoader, verbose=False):
             torch.distributed.reduce(total_correct, 0, op=torch.distributed.ReduceOp.SUM)
             torch.distributed.reduce(example_count, 0, op=torch.distributed.ReduceOp.SUM)
 
+        print(total_loss)
+        print(total_loss.item())
+
         total_loss = total_loss.cpu().item()
         total_correct = total_correct.cpu().item()
         example_count = example_count.cpu().item()
